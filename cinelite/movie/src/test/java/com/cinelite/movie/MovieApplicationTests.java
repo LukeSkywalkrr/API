4ebdc358d896b1a30495af2d77bd8eb2b868e0b8
Pratyush Kumar Tiwari <60344989+LukeSkywalkrr@users.noreply.github.com>
2025-11-02 07:39:51 +0000

    chore: upgrade parent to Spring Boot 3.5.0 and convert cinelite to normal directory (backup nested .git)

---
+++ cinelite/movie/src/test/java/com/cinelite/movie/MovieApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cinelite.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
